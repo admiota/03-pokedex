0e3dc59017ed20c7cf584276e6be70d92ad011c0
adolfo <adolfomiota1@gmail.com>
2024-03-19 19:00:33 +0100

    envs service(ConfigService)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,21 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }), //Para detectar las .envs
+    ServeStaticModule.forRoot({ //Para servir contenido estático
       rootPath: join(__dirname, '..', 'public')
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB), //Para establecer conexión con la bbdd de mongo
     PokemonModule,
     CommonModule,
     SeedModule
-  ],
+  ]
 })
 export class AppModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,17 +4,20 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
-import { isString } from 'class-validator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
+
   constructor(
-
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
+  ) {
+    this.defaultLimit = this.configService.get('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -27,7 +30,8 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit=10, offset=0 } = paginationDto;
+     
+    const { limit=this.defaultLimit, offset=0 } = paginationDto;
     return this.pokemonModel.find()
       .limit(limit)
       .skip(offset)
